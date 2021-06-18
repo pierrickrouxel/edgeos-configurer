712f73fdc2c096ae8993bef80a4fc06a9d26bc47
Pierrick Rouxel <pierrick.rouxel@inconcept.fr>
2021-06-18 14:26:46 +0200

    Initial Commit

---
+++ Shared/EdgeOsConfigurerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeOsConfigurerApp.swift
+//  Shared
+//
+//  Created by Pierrick Rouxel on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdgeOsConfigurerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
