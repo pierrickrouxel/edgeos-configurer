82f14810d9f05854e548fcf1c6bb9d87966f58fb
Pierrick Rouxel <pierrick.rouxel@inconcept.fr>
2021-06-22 17:17:48 +0200

    Fullscreen ui

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,23 +11,32 @@ struct ContentView: View {
     @ObservedObject private var orangeAccountModel = OrangeAccountModel()
     
     var body: some View {
-        Form {
-            ScrollView {
-                TextField("Login (starts with fti/)", text: $orangeAccountModel.login);
-                TextField("Password", text: $orangeAccountModel.password)
-                TextField("Salt", text: $orangeAccountModel.salt)
-                TextField("Byte", text: $orangeAccountModel.byte)
-                Button("Generate", action: generate)
-                Section(header: Text("DHCP Option 90")) {
-                    Button("Copy", action: {
-                        NSPasteboard.general.clearContents()
-                        NSPasteboard.general.setString(orangeAccountModel.option90, forType: .string)
-                    })
-                    Text(orangeAccountModel.option90)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .padding()
+        HStack {
+            Form {
+                ScrollView {
+                    TextField("Login (starts with fti/)", text: $orangeAccountModel.login)
+                    TextField("Password", text: $orangeAccountModel.password)
+                    TextField("Salt", text: $orangeAccountModel.salt)
+                    TextField("Byte", text: $orangeAccountModel.byte)
+                    Button("Generate", action: generate)
+                    Section(header: Text("DHCP Option 90")) {
+                        Button("Copy", action: {
+                            NSPasteboard.general.clearContents()
+                            NSPasteboard.general.setString(orangeAccountModel.option90, forType: .string)
+                        })
+                        Text(orangeAccountModel.option90)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .padding()
+                    }
                 }
+                .frame(width: 300.0)
             }
+            .padding()
+            .padding(.top, 28)
+            VStack {
+                Color.black
+            }
+            .frame(minWidth: 300, idealWidth: 500, maxWidth: .infinity, minHeight: 0, idealHeight: 100, maxHeight: .infinity, alignment: .leading)
         }
     }
     
--- Shared/EdgeOsConfigurerApp.swift
+++ Shared/EdgeOsConfigurerApp.swift
@@ -12,6 +12,8 @@ struct EdgeOsConfigurerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .edgesIgnoringSafeArea(.top)
         }
+        .windowStyle(HiddenTitleBarWindowStyle())
     }
 }
